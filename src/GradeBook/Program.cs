86e7a107145fe64c0c654da2fe1e59dc99f01a8b
Ransford Walker <ransfordw@gmail.com>
2019-07-24 11:36:21 -0400

    Added a simple loop to calculate sum and average of a collection of numbers.

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -6,6 +7,16 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+            var grades = new List<double> { 12.7, 34.1, 73.2 ,45.5, 23.3};
+            var result = 0.0;
+
+            foreach (var grade in grades)
+                result += grade;
+
+            var avg = result / grades.Count;
+
+            Console.WriteLine($"The average grade is {avg:N1}.");
+
             if (args.Length > 0)
                 Console.WriteLine($"Hello, {args[0]}!");
             else
